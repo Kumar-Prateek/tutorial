ef27a6a12ad50a310b83192b0920a0a877e44d50
Prateek Kumar <prateek.kumar@rapipay.com>
2022-09-27 18:09:41 +0530

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import "./index.css";
 const App = () => (
   <div className="container">
     <div>Name: Tutorial App</div>
-    <div>Framework: react</div>
+    <div>Framework: React</div>
     <div>Language: JavaScript</div>
     <div>CSS: Empty CSS</div>
     <div>Developer: Prateek</div>
+    <div>Deployed on: Jenkins</div>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
