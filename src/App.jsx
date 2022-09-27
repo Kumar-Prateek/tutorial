9a98736f22b3fad40ce31c2c159f9e3906deee53
Prateek Kumar <prateek.kumar@rapipay.com>
2022-09-27 17:40:46 +0530

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 const App = () => (
   <div className="container">
-    <div>Name: '</div>
+    <div>Name: Tutorial App</div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Empty CSS</div>
